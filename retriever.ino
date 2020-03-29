fe8ff8ded2062e375ac8a33f79d92d3a8a94c36b
Unmesh <34339038+patilunmesh@users.noreply.github.com>
2020-03-29 20:36:52 +0530

    temp and humidity

--- retriever.ino
+++ retriever.ino
@@ -1,61 +1,31 @@
 #include <EEPROM.h>
-
 int a = 0;
 int value0;
 int value;
-
+int k = -1;
 void setup()
-{
-  Serial.begin(9600);
-}
-
+{ Serial.begin(9600); }
 void loop()
 {
   Serial.print(a);
   Serial.print("\t");
-  value0 = EEPROM.read(a);
-  Serial.print(value0);
-  for (int i = 1 ; i < 5 ; i++)
-    {
-      if((a + i) >= EEPROM.length()) {
+ while(a < EEPROM.length()) {
+      Serial.print(EEPROM.read(a));
+      Serial.print("\t");
+      a++;
+      Serial.print(EEPROM.read(a));
+      Serial.print("\t\t");
+      if(a == k + 10){
+        Serial.println();
+        k = a; }
+      a++;    }
+  if(a >= EEPROM.length()) {
         Serial.print("memory limit exceeded while reading, clearing memory");
         delay(2000);
     for (int i = 0 ; i < EEPROM.length() ; i++)
     {
     EEPROM.write(i, 0);
     }
-    Serial.print("cleared memory");
-        }
-  if(a < EEPROM.length()) {
-    a = a + i;
-  value = EEPROM.read(a);
-  Serial.print("\t");
-  Serial.print(value);
-    }
-    }
-  Serial.println();
-  if (value == 255 ){
-    Serial.print("reached limit so abort, clearing the memory");
-    delay(2000);
-    for (int i = 0 ; i < EEPROM.length() ; i++)
-    {
-    EEPROM.write(i, 0);
-    }
-    Serial.print("cleared memory");
-  }
-  
-  a = a + 1;
-
-  if (a == EEPROM.length()){
-    Serial.print("memory limit exceeded, clearing memory");
-    delay(2000);
-    for (int i = 0 ; i < EEPROM.length() ; i++)
-    {
-    EEPROM.write(i, 0);
-    }
-    Serial.print("cleared memory");
-    a = 0;
-  }
-
-  delay(10);
+    Serial.print("cleared memory");  }
+ delay(10);
 }
